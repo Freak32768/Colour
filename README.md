96163edb67cde0b001d3a1fa630205f3b28d3028
Freak32768 <freak32768@gmail.com>
2021-06-23 19:50:07 +0900

    Add enemy manage system and shooting system

--- README.md
+++ README.md
@@ -1,48 +1,47 @@
 
 # Table of Contents
 
-1.  [Introduction](#org3b750c0)
-2.  [System](#org793a503)
-3.  [TODO-LIST](#org314e612)
-4.  [DONE-LIST](#orgaa4ad4f)
-5.  [REMOVED-LIST](#org13e69b2)
-6.  [License](#org7633bf2)
+1.  [Introduction](#orga22a510)
+2.  [System](#org4ec9202)
+3.  [TODO-LIST](#orgb3801e7)
+4.  [DONE-LIST](#org29e0cae)
+5.  [REMOVED-LIST](#orgea6569a)
+6.  [License](#org34d92c3)
 
 
 
-<a id="org3b750c0"></a>
+<a id="orga22a510"></a>
 
 # Introduction
 
 -   Colour(コロールと読む)はその名の通り"色"をテーマにした
     3DパズルTPSゲームです。(パズルかどうかは微妙です。)
 -   ColourはGauche(Scheme処理系), Gauche-gl(GaucheのOpenGL拡張)を必要とします。
+-   コンピュータによっては挙動がおかしくなることがあります。ご了承ください。
 
 
-<a id="org793a503"></a>
+<a id="org4ec9202"></a>
 
 # System
 
--   ADで左右に方向回転、WSで移動です。
--   Vで弾を選択し、Bで弾を発射します。
+-   ADで左右に方向回転、WSで前後移動です。
+-   Vで弾の色を選択し、Bで弾を発射します。
 
 
-<a id="org314e612"></a>
+<a id="orgb3801e7"></a>
 
 # TODO-LIST
 
-これから実装予定の機能です。
+これから実装予定の機能です
 
--   ライトの調整(今はとても暗い)
--   効率的な敵の管理システム
--   シューティング機能
+-   リファクタリング(今はコードがごちゃごちゃしている)
 -   ダメージ
 -   構造物
 -   テクスチャ
 -   ゲームシステム
 
 
-<a id="orgaa4ad4f"></a>
+<a id="org29e0cae"></a>
 
 # DONE-LIST
 
@@ -54,24 +53,27 @@
 -   3Dモデルロードシステム -2021/6/16
 -   自機モデル -2021/6/18
 -   敵のモデル、移動システム -2021/6/22
+-   効率的な敵の管理システム -2021/6/23
+-   シューティング機能 -2021/6/23
 
 
-<a id="org13e69b2"></a>
+<a id="orgea6569a"></a>
 
 # REMOVED-LIST
 
-削除された機能です。
+削除された、または実装を諦めた機能です。
 
 -   WASDによる移動 -移動システムの改良により削除
+-   ライトの調整(今はとても暗い) -どうしてもわからなかった
 
 
-<a id="org7633bf2"></a>
+<a id="org34d92c3"></a>
 
 # License
 
 -   Colour is licensed under GPL v3 or later.
     See the COPYING file or [GNU-website](https://www.gnu.org/licenses).
--   README.md and 3D model files in ./objects
+-   README.md and 3D model files in ./objects/
     (\*.blend, \*.obj) are licensed under CC0,
     so you can do anything what you want to do.
     See [CC0-website](https://creativecommons.org/choose/zero/).
--- main.scm
+++ main.scm
@@ -20,40 +20,54 @@ This file is part of Colour.
 
 (use gl)
 (use gl.glut)
-(load "./3d-object.scm")
-(load "./trifunc.scm")
+(load "./libs/3d-object.scm")
+(load "./libs/trifunc.scm")
+
+(define-class <rifle> ()
+  ((bullet-color :init-value #f32(0 1 0 1)
+                 :init-keyword :bullet-color
+                 :accessor bullet-color-of)
+   (cooltime :init-value 0
+             :init-keyword :cooltime
+             :accessor cooltime-of)))
 
 ;;define constant values
 (define *window-width* 640)
 (define *window-height* 640)
-(define *floor-size* 10)
+(define *floor-size* 20)
 (define *floor-color* #f32(0.5 0.5 0.5 1))
 (define *light0-pos* #f32(5 3 5 1))
 (define *light1-pos* #f32(-5 2 -5 1))
 (define *light-color* #f32(1 1 1 0))
 
-;;define dynamic variables
 (define *player* (make <3d-obj>
                    :x 0 :y 0 :z 0
                    :color #f32(0 1 0 1)
                    :obj-data (load-obj "./objects/player.obj")
                    ))
-
+(define *player-rifle* (make <rifle>))
 (define *enemies* '())
-
+(define *bullets* '())
 (define *keycode* #\null)
 
-(define (spawn-enemy! x y z color)
-  (set! *enemies* (cons (make <3d-obj>
-                          :x x :y y :z z
-                          :color color
-                          :obj-data (load-obj "./objects/enemy.obj"))
-                        *enemies*))
-  )
-(spawn-enemy! 5 0 5 #f32(1 0 0 1))
-(spawn-enemy! -5 0 -5 #f32(0 0 1 1))
+(define (spawn-obj new-obj obj-list)
+  (if (< 10 (length obj-list))
+      (cons new-obj (remove-invisible-obj obj-list))
+      (cons new-obj obj-list)) )
 
-(define (display)
+(define (remove-invisible-obj obj-list)
+  (remove (lambda (obj) (eq? (visible-of obj) #f)) obj-list) )
+
+(set! *enemies* (spawn-obj (make <3d-obj> :x 5 :y 0 :z 5
+                                 :color #f32(1 0 0 1)
+                                 :obj-data (load-obj "./objects/enemy.obj"))
+                           *enemies*))
+(set! *enemies* (spawn-obj (make <3d-obj> :x -5 :y 0 :z -5
+                                 :color #f32(0 0 1 1)
+                                 :obj-data (load-obj "./objects/enemy.obj"))
+                           *enemies*))
+
+(define (display-main-scene)
   (gl-clear (logior GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
   (gl-light GL_LIGHT0 GL_POSITION *light0-pos*)
   (display-canvas)
@@ -63,7 +77,8 @@ This file is part of Colour.
 (define (display-canvas)
   (display-floor)
   (move-player!)
-  (move-obj-list! *enemies*)
+  (move-obj-list! *enemies* move-enemy!)
+  (move-obj-list! *bullets* move-bullet!)
   (display-camera)
   )
 
@@ -72,7 +87,7 @@ This file is part of Colour.
   (gl-load-identity)
   (glu-look-at
    (- (x-of *player*) (* (table-sin (r-of *player*)) 3))
-   (+ (y-of *player*) (+ (y-of *player*) 1))
+   (+ (y-of *player*) (+ (y-of *player*) 1.25))
    (- (z-of *player*) (* (table-cos (r-of *player*)) 3))
    (x-of *player*) (+ (y-of *player*) 0.5) (z-of *player*)
    0 1 0)
@@ -96,39 +111,81 @@ This file is part of Colour.
    ( (char=? *keycode* #\a)
      (set! (r-of *player*) (if (<= 360 (r-of *player*))
                                0
-                               (+ (r-of *player*) 4)) ))
+                               (+ (r-of *player*) 3)) ))
    ( (char=? *keycode* #\d)
      (set! (r-of *player*) (if (>= 0 (r-of *player*))
                                360
-                               (- (r-of *player*) 4)) ))
+                               (- (r-of *player*) 3)) ))
+   ( (char=? *keycode* #\w) (move-obj! *player* 0.1) )
+   ( (char=? *keycode* #\s) (move-obj! *player* -0.03) )
+   ( (char=? *keycode* #\b) (shoot-bullet: *player-rifle*) )
    )
-  (cond ( (char=? *keycode* #\w) (move-obj! *player* 0.1) )
-        ( (char=? *keycode* #\s) (move-obj! *player* -0.03) )
-        )
   (display-3d-obj: *player*)
   )
 
-(define (move-obj-list! obj-list)
-  (for-each (lambda (x) (move-enemy! x)) obj-list)
+(define (move-obj-list! obj-list obj-func)
+  (for-each (lambda (x) (obj-func x)) obj-list)
   )
 
-(define-method move-enemy! ((obj <3d-obj>))
+(define-method collide-with-others? ((obj <3d-obj>) obj-list)
+  (not (null? (filter
+               (lambda (other-obj)
+                 (and (< (distance: obj other-obj) 0.2)
+                      (visible-of other-obj)) )
+               (remove (lambda (x) (equal? x obj)) obj-list) )
+              )))
+
+(define-method move-enemy! ((enemy <3d-obj>))
   (let ((facing
          (exact (floor (rad-to-deg
-                        (atan (- (x-of *player*) (x-of obj))
-                              (- (z-of *player*) (z-of obj)))
+                        (atan (- (x-of *player*) (x-of enemy))
+                              (- (z-of *player*) (z-of enemy)))
                  )))
          ))
-    (set! (r-of obj)
+    (when (or (collide-with-others? enemy *enemies*)
+              (collide-with-others? enemy *bullets*))
+      (set! (visible-of enemy) #f))
+    (set! (r-of enemy)
           (cond ( (< facing 0) (+ facing 360) )
                 ( (> facing 360) (- facing 360) )
                 ( else facing )
                 ) )
-    )
-  (when (> (distance: *player* obj) 0.3)
-    (move-obj! obj 0.02))
-  (display-3d-obj: obj)
+    (when (> (distance: *player* enemy) 0.2)
+      (move-obj! enemy 0.03))
+    (display-3d-obj: enemy)
+    ))
+
+(define-method shoot-bullet: ((rifle <rifle>))
+  (if (< (cooltime-of rifle) 0)
+      (begin
+        (set! *bullets* (spawn-obj (make <3d-obj>
+                                     :x (x-of *player*)
+                                     :y (+ (y-of *player*) 0.5)
+                                     :z (z-of *player*)
+                                     :r (r-of *player*)
+                                     :color (bullet-color-of rifle)
+                                     :obj-data (load-obj "./objects/bullet.obj")
+                                     ) *bullets*))
+        (set! (cooltime-of rifle) 5)
+        )
+      (when (< -1 (cooltime-of rifle))
+          (set! (cooltime-of rifle) (- (cooltime-of rifle) 1)) )
+      )
   )
+
+(define-method out-of-map? ((obj <3d-obj>))
+  (or (< (/ *floor-size* 2) (x-of obj) )
+      (< (x-of obj) (/ (- *floor-size*) 2) )
+      (< (/ *floor-size* 2) (z-of obj) )
+      (< (z-of obj) (/ (- *floor-size*) 2) )
+      ))
+(define-method move-bullet! ((bullet <3d-obj>))
+  (move-obj! bullet 0.2)
+  (when (out-of-map? bullet)
+    (set! (visible-of bullet) #f))
+  (display-3d-obj: bullet)
+  )
+
 
 (define-method move-obj! ((obj <3d-obj>) d)
   (set! (x-of obj)
@@ -150,7 +207,7 @@ This file is part of Colour.
   )
 
 (define (keyboard key x y)
-  (if (= key (char->integer #\q))
+  (if (= key (char->integer #\ESCAPE))
          (exit 0)
          (set! *keycode* (integer->char key))
          ))
@@ -174,7 +231,7 @@ This file is part of Colour.
   (glut-init-window-size *window-width* *window-height*)
   (glut-init-display-mode (logior GLUT_RGBA GLUT_DOUBLE GLUT_DEPTH))
   (glut-create-window "Colour")
-  (glut-display-func display)
+  (glut-display-func display-main-scene)
   (glut-reshape-func resize)
   (glut-keyboard-func keyboard)
   (glut-keyboard-up-func key-up)
